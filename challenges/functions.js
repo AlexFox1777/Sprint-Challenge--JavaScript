8ebcd061af8dd26ea130daceed2c54f728f0af11
Sasha <mix.filipov@mail.ru>
2019-06-14 10:07:56 -0700

    MVP

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,25 +7,38 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(arg1, arg2, callback) {
+  return callback(arg1,arg2);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
+function add(num1, num2) {
+  return num1 + num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function greeting(firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
-
+const result1 = consume(2,2,add); // 4
+const result2 = consume(10,16,multiply); // 160
+const result3 = consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(result1, result2, result3);
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation:  functions have ability look outward for context and never vice versa.
+// If function can`t find variable inside, it looks outward at parent functions till find needed context.
 
 
 const external = "I'm outside the function";
